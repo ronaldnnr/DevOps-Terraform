ee46f93277f493ae81ffe4b08ff528815e36a2e8
ronaldnnr <46574677+ronaldnnr@users.noreply.github.com>
2020-06-12 08:37:24 -0700

    Update install-gitlabrunner.sh

---
+++ install-gitlabrunner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Install GitLab Runner
+
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
